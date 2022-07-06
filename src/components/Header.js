55989f0cc4a8ea05d09a716a0c3952f39dbb9445
anton startsev <anton.startsev@osome.com>
2022-07-06 14:00:50 +0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from "../images/logo-w.svg";
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <header className="header">
             <img src={logo} alt="логотип" className="header__logo"/>
+            <div style={{display: "flex"}}>
+            <p style={{margin: 0, color: "red"}}>test@test.test</p>
+            <Link to="/sign-up" style={{marginLeft: "24px"}}>Регистрация</Link>
+            </div>
         </header>
     )
 }
