a2630eff5669b3fd944d8185d783d05ad0886d65
anton startsev <anton.startsev@osome.com>
2022-07-06 12:11:22 +0300

    popup res

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import {Route, Routes} from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
+import InfoTooltip from "./InfoTooltip";
 
 
 function App() {
@@ -123,6 +124,7 @@ function App() {
             <PopupWithForm title='Вы уверены?' name='delete' />
             <EditAvatarPopup isOpen={isEditAvatarOpen} onOpen={closeAllPopups} onUpdateAvatar={handleUpdateAvatar} />
             <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
+            <InfoTooltip onClose={closeAllPopups}></InfoTooltip>
 
             <Footer />
           </div>
---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,22 @@
+import React from "react";
+import res from "../images/res.svg";
+import err from "../images/err.svg";
+
+function InfoTooltip ({onClose}) {
+
+    return (
+        <div className="popup popup_type_active">
+            <div className="popup__container">
+                <button
+                    type="button"
+                    className="popup__close-btn"
+                    onClick={onClose}
+                />
+                <img className="popup__image" src={res}/>
+                <h2 className="popup__text">Вы успешно зарегистрировались! Что-то пошло не так! Попробуйте ещё раз.</h2>
+            </div>
+        </div>
+    )
+}
+
+export default InfoTooltip;
