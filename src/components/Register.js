65c6e93cce14a278c001fdebfadc56d5f8558955
anton startsev <anton.startsev@osome.com>
2022-07-07 11:57:36 +0300

    register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,24 +1,49 @@
 import React from "react";
+import {register} from "../utils/auth.js";
 import {Link} from "react-router-dom";
 
 function Register () {
+
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    function handleEmailChange(event) {
+        setEmail(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        register(
+            password,
+            email
+        );
+    }
+
     return (
         <div className="popup__container">
-        <form className="popup__form" name="register">
+        <form onSubmit={handleSubmit} className="popup__form" name="register">
             <h1>Регистрация</h1>
             <input
                 type="email"
                 placeholder="Email"
                 required
                 name="email"
+                value={email || ''}
+                onChange={handleEmailChange}
             />
             <input
                 type="text"
                 placeholder="Пароль"
                 required
-                name="password"
+                name="passwordword"
                 minLength="8"
                 maxLength="20"
+                value={password || ''}
+                onChange={handlePasswordChange}
             />
             <button type="submit">Зарегистрироваться</button>
             <p>Уже зарегистрированы?<Link to="/sign-in">Войти</Link></p>
---
+++ src/utils/auth.js
@@ -0,0 +1,18 @@
+export const BASE_URL = 'https://auth.nomoreparties.co';
+
+export const register = (password, email) => {
+    return fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({password, email})
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((res) => {
+            return res;
+        })
+        .catch((err) => console.log(err));
+};
